92653bbd80adaa1840f6dc4d188ab87c140ed6ea
sdemming1 <35880426+sdemming1@users.noreply.github.com>
2018-01-28 20:43:14 -0500

    Add files via upload
    Added in NanoMouseSensors.h file and updated main

--- NanoMouse.ino
+++ NanoMouse.ino
@@ -1,11 +1,15 @@
 #include <Servo.h>
 #include "NanoMouseMotors.h"
+#include "NanoMouseSensors.h"
 
 const byte ledPin = 13;
 const byte buttonPin = 9;
 
 
 NanoMouseMotors motors;
+
+//<3,A6> is the front emitter and front detector
+NanoMouseSensors<3,A6> sensors; //A6 is passed as front detector
 
 //Main
 void setup() 
@@ -15,16 +19,21 @@ void setup()
   pinMode(ledPin,OUTPUT);
   pinMode(buttonPin,INPUT_PULLUP);
 
+  sensors.configure();
+
+  Serial.begin(9600);
+  
   while(digitalRead(buttonPin))
   {
   }
   
-  motors.square(RIGHT,2);
+  //motors.square(RIGHT,2);
 }
 
 void loop() 
 {
 
-  digitalWrite(ledPin,HIGH);
-  
+  //digitalWrite(ledPin,HIGH);
+  sensors.view();
+  delay(100);
 }
