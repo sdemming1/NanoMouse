561300d15be5d037ff3ed32ccd8b4b3fdc85dc37
sdemming1 <35880426+sdemming1@users.noreply.github.com>
2018-01-28 19:28:47 -0500

    Add files via upload

---
+++ NanoMouse.ino
@@ -0,0 +1,30 @@
+#include <Servo.h>
+#include "NanoMouseMotors.h"
+
+const byte ledPin = 13;
+const byte buttonPin = 9;
+
+
+NanoMouseMotors motors;
+
+//Main
+void setup() 
+{
+  motors.attach(6,5);
+  
+  pinMode(ledPin,OUTPUT);
+  pinMode(buttonPin,INPUT_PULLUP);
+
+  while(digitalRead(buttonPin))
+  {
+  }
+  
+  motors.square(RIGHT,2);
+}
+
+void loop() 
+{
+
+  digitalWrite(ledPin,HIGH);
+  
+}
---
+++ NanoMouseMotors.h
@@ -0,0 +1,69 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+#define RIGHT 1
+#define LEFT -1
+
+class NanoMouseMotors
+{
+private:
+    Servo leftServo;
+    Servo rightServo;
+
+    static const byte power = 250;
+    static const byte ii = 1;
+
+public:
+
+    void attach(byte leftMotor,byte rightMotor)
+    {
+
+        leftServo.attach(leftMotor);
+        rightServo.attach(rightMotor);
+      
+    }
+    void stop(int StopTime)
+    {
+
+      leftServo.writeMicroseconds(1500);
+      rightServo.writeMicroseconds(1500);
+      delay(StopTime);
+
+    }
+
+    void forwardTime(unsigned int time)
+    {
+
+      leftServo.writeMicroseconds(1500 - power);
+      rightServo.writeMicroseconds(1500 + power);
+      delay(time);
+      stop(200);
+
+    }
+
+    void turn(int direction, int degrees)
+    {
+
+      leftServo.writeMicroseconds(1500 - power * direction);
+      rightServo.writeMicroseconds(1500 - power * direction);
+      delay(degrees * 5.9);
+      stop(200);
+    }
+
+    void square(int direction, int size)
+    //Size is a single integer that is multiplied by 1000
+    {
+
+      for (int ii = 1; ii < 5; ii++)
+      {
+        forwardTime(size * 1000);
+        turn(direction, 90);
+        delay(200);
+        ii++;
+      }
+
+    }
+
+}; //Semicolon for classes
+
+
